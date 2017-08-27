2df3922c6ce653d0aab3390445c9989b195e5433
Taras Romanchuk <romanchuk.taras@gmail.com>
2017-08-27 22:09:17 +0300

    Added prefabs and designed first level

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+	public int hitsToKill;
+	public int points;
+	private int numberOfHits;
+
+	void Start()
+	{
+		numberOfHits = 0;
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ball")
+		{
+			numberOfHits++;
+
+			if (numberOfHits == hitsToKill)
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
